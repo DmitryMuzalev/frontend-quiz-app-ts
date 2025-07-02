52eec4ca36dc9f853be82dc4509fbc363e44ec00
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-07-02 23:04:48 +0300

    refactor: Remove unused Button, Container, and Header components and their styles

--- src/components/Container/Container.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Container.module.scss";
-
-interface ContainerProps {
-  children: React.ReactNode;
-}
-
-export const Container = ({ children }: ContainerProps) => (
-  <div className={styles.container}>{children}</div>
-);
