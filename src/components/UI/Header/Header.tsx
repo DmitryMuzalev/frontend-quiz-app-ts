aeb7be6049020058d061eb42772ac4179fb7b61d
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-07-02 23:06:09 +0300

    feat: Implement Button, Header, Layout, and ThemeSwitcher components with styles

---
+++ src/components/UI/Header/Header.tsx
@@ -0,0 +1,24 @@
+import { ThemeSwitcher } from "components/UI/ThemeSwitcher/ThemeSwitcher";
+import styles from "./Header.module.scss";
+import { useContextApp } from "hook/useContextApp";
+import { LogoQuiz } from "components/LogoQuiz/LogoQuiz";
+import { useLocation } from "react-router-dom";
+
+export const Header = () => {
+  const { currentQuiz } = useContextApp();
+  const location = useLocation();
+  const path = location.pathname;
+
+  return (
+    <header className={styles.header}>
+      {path !== "/" && currentQuiz ? (
+        <LogoQuiz images={currentQuiz.icon} name={currentQuiz.title} />
+      ) : (
+        <div></div>
+      )}
+
+      {/*  */}
+      <ThemeSwitcher />
+    </header>
+  );
+};
---
+++ src/components/UI/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import { Header } from "components/UI/Header/Header";
+import styles from "./Layout.module.scss";
+
+export const Layout = () => {
+  return (
+    <>
+      <Header />
+      <div className={styles.layout}>
+        <Outlet />
+      </div>
+    </>
+  );
+};
